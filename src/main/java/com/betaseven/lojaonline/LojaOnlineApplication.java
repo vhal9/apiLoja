ce6175a27515ba709bb095e8c6609f3ff9452af7
Victor Hugo Landin <victor.h.landin@gmail.com>
2023-02-05 11:07:19 -0300

    inicializando projeto

---
+++ src/main/java/com/betaseven/lojaonline/LojaOnlineApplication.java
@@ -0,0 +1,15 @@
+package com.betaseven.lojaonline;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LojaOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/betaseven/lojaonline/LojaOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betaseven.lojaonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
