ce0cb44f95a5d48653e01d2c316bd28018fdc1e0
Victor Hugo Landin <victor.h.landin@gmail.com>
2023-02-11 21:50:46 -0300

    Adiciona arquivos de propriedades para ambientes diferentes

--- src/main/java/com/betaseven/lojaonline/scheduler/ProcessamentoDeCompraScheduler.java
+++ src/main/java/com/betaseven/lojaonline/scheduler/ProcessamentoDeCompraScheduler.java
@@ -4,6 +4,7 @@ import com.betaseven.lojaonline.service.ProcessamentoDeCompraService;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -11,12 +12,13 @@ import org.springframework.stereotype.Service;
 @Service
 @EnableScheduling
 @RequiredArgsConstructor
+@Profile(value = "!local")
 public class ProcessamentoDeCompraScheduler {
 
     private final ProcessamentoDeCompraService processamentoDeCompraService;
     private final Logger logger = LogManager.getLogger(ProcessamentoDeCompraScheduler.class);
 
-    @Scheduled(cron = "${cron.processamento.compra.pendente}", zone = "America/Sao_Paulo")
+    @Scheduled(cron = "${processamento.compra.pendente.cron}", zone = "${processamento.compra.pendente.zone}")
     public void ProcessamentoDeComprasPendentes() {
         try {
             logger.info("Job ProcessamentoDeComprasPendentes - iniciado");
