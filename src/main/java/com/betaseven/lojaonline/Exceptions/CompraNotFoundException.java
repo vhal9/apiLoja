474c01f6271caafbb0529f562247269f42c5923b
Victor Hugo Landin <victor.h.landin@gmail.com>
2024-01-27 11:30:16 -0300

    add controller advice para tratar exceçoes

---
+++ src/main/java/com/betaseven/lojaonline/Exceptions/CompraNotFoundException.java
@@ -0,0 +1,7 @@
+package com.betaseven.lojaonline.Exceptions;
+
+public class CompraNotFoundException extends RuntimeException {
+    public CompraNotFoundException(String s) {
+        super(s);
+    }
+}
---
+++ src/main/java/com/betaseven/lojaonline/controller/ApplicationControllerAdvice.java
@@ -0,0 +1,49 @@
+package com.betaseven.lojaonline.controller;
+
+import com.betaseven.lojaonline.Exceptions.CompraNotFoundException;
+import com.betaseven.lojaonline.domain.dtos.erros.RestErrorMessageDTO;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    private ResponseEntity<RestErrorMessageDTO> methodArgumentNotValidExceptionHlander(MethodArgumentNotValidException e) {
+        List<String> erros = e.getBindingResult().getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+        RestErrorMessageDTO restErrorMessageDTO = RestErrorMessageDTO.builder()
+                .messages(erros)
+                .status(HttpStatus.BAD_REQUEST)
+                .build();
+        return ResponseEntity.status(restErrorMessageDTO.getStatus()).body(restErrorMessageDTO);
+    }
+
+    @ExceptionHandler(CompraNotFoundException.class)
+    private ResponseEntity<RestErrorMessageDTO> compraNotFoundExceptionHandler(CompraNotFoundException e) {
+        RestErrorMessageDTO restErrorMessageDTO = RestErrorMessageDTO.builder()
+                .status(HttpStatus.NOT_FOUND)
+                .messages(List.of(e.getMessage()))
+                .build();
+        return ResponseEntity.status(restErrorMessageDTO.getStatus()).body(restErrorMessageDTO);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    private ResponseEntity<RestErrorMessageDTO> runtimeExceptionHanlder(RuntimeException e) {
+        RestErrorMessageDTO restErrorMessageDTO = RestErrorMessageDTO.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .messages(List.of(e.getMessage()))
+                .build();
+        return ResponseEntity.status(restErrorMessageDTO.getStatus()).body(restErrorMessageDTO);
+    }
+
+}
