d8a5f8fe59a7d2cf5c3d78be6edc9d5b413fc613
Victor Hugo Landin <victor.h.landin@gmail.com>
2024-02-04 08:40:53 -0300

    adiciona validaçao para criar uma compra

--- src/main/java/com/betaseven/lojaonline/service/impl/CompraServiceImpl.java
+++ src/main/java/com/betaseven/lojaonline/service/impl/CompraServiceImpl.java
@@ -1,11 +1,15 @@
 package com.betaseven.lojaonline.service.impl;
 
+import com.betaseven.lojaonline.Exceptions.ClienteNotFoundException;
 import com.betaseven.lojaonline.Exceptions.CompraNotFoundException;
+import com.betaseven.lojaonline.Exceptions.UnauthorizedException;
+import com.betaseven.lojaonline.domain.Enum.Role;
 import com.betaseven.lojaonline.domain.Enum.StatusCompraEnum;
 import com.betaseven.lojaonline.domain.dtos.CompraDTO;
 import com.betaseven.lojaonline.domain.mappers.CompraDTOMapper;
 import com.betaseven.lojaonline.domain.model.Cliente;
 import com.betaseven.lojaonline.domain.model.Compra;
+import com.betaseven.lojaonline.domain.model.Usuario;
 import com.betaseven.lojaonline.repositories.CompraRepository;
 import com.betaseven.lojaonline.service.ClienteService;
 import com.betaseven.lojaonline.service.CompraService;
@@ -18,6 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -31,19 +36,15 @@ public class CompraServiceImpl implements CompraService {
     private final ItemCompraService itemCompraService;
     private final CompraDTOMapper compraDTOMapper;
     private final Logger logger = LogManager.getLogger(CompraServiceImpl.class);
+    private final TokenServiceImpl tokenService;
 
 
     @Override
     @Transactional
-    public CompraDTO inserirCompra(CompraDTO compraDTO) throws Exception {
+    public CompraDTO inserirCompra(CompraDTO compraDTO) {
         try {
 
-            Cliente cliente = clienteService.buscarCliente(compraDTO.idCliente);
-
-            Compra compra = new Compra();
-            compra.setCliente(cliente);
-            compra.setDataCompra(LocalDateTime.now());
-            compra.setStatusCompra(StatusCompraEnum.PENDENTE);
+            Compra compra = validarCompra(compraDTO);
 
             logger.info("Inserindo a compra: " +  compra);
             Compra compraSalva = compraRepository.save(compra);
@@ -102,4 +103,32 @@ public class CompraServiceImpl implements CompraService {
         compraRepository.finalizarProcessamentoDaCompra(idCompra, StatusCompraEnum.ERRO, LocalDateTime.now());
     }
 
+    private Compra validarCompra(CompraDTO compraDTO) {
+        try {
+            Usuario usuarioLogado = tokenService.getUsuarioFromSession();
+            if (Objects.equals(usuarioLogado.getId(),compraDTO.getIdUsuario()) || Objects.isNull(compraDTO.getIdUsuario())) {
+                compraDTO.setIdUsuario(usuarioLogado.getId());
+            } else if (!Objects.equals(usuarioLogado.getRole(), Role.ADMIN)) {
+                throw new UnauthorizedException();
+            }
+            // todo: validar itens de compra
+            Cliente cliente = clienteService.buscarCliente(compraDTO.getIdCliente())
+                    .orElseThrow(() -> new ClienteNotFoundException());
+
+            Compra compra = new Compra();
+            compra.setCliente(cliente);
+            compra.setDataCompra(LocalDateTime.now());
+            compra.setStatusCompra(StatusCompraEnum.PENDENTE);
+            return compra;
+        } catch (UnauthorizedException e) {
+            logger.error("Erro ao validar compra - Usuario nao tem acesso ao recurso");
+            throw e;
+        } catch (ClienteNotFoundException e) {
+            logger.error("Erro ao validar compra - Cliente nao encontrado");
+            throw e;
+        } catch (Exception e) {
+            logger.error("Erro ao validar compra: ", e);
+            throw e;
+        }
+    }
 }
